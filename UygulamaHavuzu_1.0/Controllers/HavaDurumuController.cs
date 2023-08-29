94d5bac0865283c545ab82c58a1c9549675a8a04
bihter <bihter_bes@hotmail.com>
2023-08-29 17:18:03 +0300

    hava durumu ve kalp

--- UygulamaHavuzu_1.0/Controllers/HavaDurumuController.cs
+++ UygulamaHavuzu_1.0/Controllers/HavaDurumuController.cs
@@ -3,11 +3,45 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net.Http;
+using UygulamaHavuzu_1._0.Models;
 
 namespace UygulamaHavuzu_1._0.Controllers
+
+
 {
     public class HavaDurumuController : Controller
     {
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public HavaDurumuController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            string apiKey = "b1ded651a30f8840194a8ae16154d8e8";
+            string cityName = "Ankara"; // İstediğiniz şehir adını buraya ekleyin
+            string apiUrl = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}";
+
+            HttpClient client = _httpClientFactory.CreateClient();
+
+            HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var weatherDataJson = await response.Content.ReadAsStringAsync();
+                var weatherData = Newtonsoft.Json.JsonConvert.DeserializeObject<WeatherModels>(weatherDataJson);
+                return View(weatherData);
+            }
+            else
+            {
+                return Content("API request failed.");
+            }
+        }
+       
         public IActionResult HavaDurumuApp()
         {
             return View();
---
+++ UygulamaHavuzu_1.0/Models/WeatherModels.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UygulamaHavuzu_1._0.Models
+{
+    public class WeatherModels
+    {
+        public string Name { get; set; } // Şehir adı
+        public double WindSpeed { get; set; } // Rüzgar hızı
+        public int Humidity { get; set; } // Nem
+        public double Temperature { get; set; } // Sıcaklık
+
+    }
+    public class Weather
+    {
+        public int id { get; set; }
+        public string main { get; set; }
+        public string description { get; set; }
+        public string icon { get; set; }
+    }
+    public class Main
+    {
+        public double temp { get; set; }
+       
+        public int humidity { get; set; }
+    }
+
+    public class Wind
+    {
+        public double speed { get; set; }
+    }
+    public class Root
+    {
+       
+        public List<Weather> weather { get; set; }
+    
+        public Main main { get; set; }
+  
+       public Wind wind { get; set; }
+    
+  
+        public string name { get; set; }
+     
+     }
+}
